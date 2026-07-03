ffab86081c6cb2776b46f8a4ca4c141d5ed9e459
Ronak Malik <reeshumalik7@gmail.com>
2026-07-03 10:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (40.33%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,24 +1,23 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-         k = k % n;
 
-        int[] result = new int[n];
-        int index = 0;
+        k = k%n;
 
-        for(int i=n-k;i<n;i++){
-            result[index++]=nums[i];
-        }
-        for(int i = 0; i<n-k; i++){
-            result[index++]=nums[i];
-        }
-
-        for(int i = 0; i<n; i++){
-            nums[i] = result[i];
-
-        }
-        //In the first two loops, we need two different pointers because we are reading from one place but writing to another. In the final loop, we only need one pointer because we are moving strictly parallel from index 0 to the end.
-
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
         
+    }
+
+    public static void reverse(int[] nums, int left, int right ){
+        while(left < right ){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left ++;
+            right--;
+        }
     }
 }
